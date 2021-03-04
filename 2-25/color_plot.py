1905928009fa0a9cff2cff6470694a413c4961fb
Charlotte Rigby <chrdaisy@gmail.com>
2021-03-04 16:54:41 -0500

    added late

--- 2-25/color_plot.py
+++ 2-25/color_plot.py
@@ -4,27 +4,33 @@ from netCDF4 import Dataset
 from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
-w_file = 'path/to/w/file' # fix this
+w_file = '/Users/brownscholar/Desktop/InternGit/ocean-motion-2021/2-25/test/ss1a2ww.gr' # fix this
 w = open(w_file, "r")
 
 #open netcdf file
-original_data = 
+original_data = Dataset('/Users/brownscholar/Desktop/BridgeUp_Year2/n-atlantic-2018.nc')
 
 #get latitude and longitude from netcdf file: 
-# here:
-lat =
-lon = 
+#lat = original_data.variable['lat'][:]
+#lon = original_data.variable['lon'][:]
 
 num_lat = 158
 num_lon = 122
 levels = 1
 
 # make empty numpy array of shape lat, lon depth shape for storing w:
+array = np.zeros((1,158,122))
 
 
 # use a loop to read w_file into the variable 
 #(skip first two lines of the file)
-
+w.readline()
+w.readline()
+for x in range(0,levels):
+	for y in range(0,num_lat):
+		for z in range(0,num_lon):
+			array[x,y,z] = w.readline()
+	
 
 
 #this stuff defines the colorspace (we can google colormaps to learn more if we want to)
@@ -40,7 +46,8 @@ newcmp = ListedColormap(newcolors, name='RedBlue')
 
 #now use the following function to plot your data:
 #function to make colorplot is:
-# p = plt.pcolormesh(V,cmap = newcmp), where V is the numpy array with the data
+p = plt.pcolormesh(array[0,2:-2,2:-2],cmap = newcmp) #where V is the numpy array with the data
+plt.show()
 
 # you can use these to add the x and y ticks to your plot:
 # I am happy to talk to you about why this works
