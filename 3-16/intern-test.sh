aa396bad516691c2ffa9f980c15b8e7978acf0c3
Charlotte Rigby <chrdaisy@gmail.com>
2021-03-23 16:51:59 -0400

    i have a lot of code running rn

--- 3-16/intern-test.sh
+++ 3-16/intern-test.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
- 
- ls 
+ cd ../2-25
+
+ STRING="START"
+ echo $STRING
+
+ gfortran -O3 -o vectorq.exe vectorq.f
+ gfortran -O3 -o omegainv.exe omegainv.f
+
+while read p; do
+  python example_vec.py $p
+  
+  ./vectorq.exec
+  ./omegainv.exec
+done <date_list.txt
+
+STRING="Done"
+echo $STRING
